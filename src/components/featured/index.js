9ce9f2b5f8a68f66a52fda66f767858bd9813cbe
gautam-11 <gautambaruah24@gmail.com>
2019-10-28 00:41:46 +0530

    Added event timer component

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Carousel from './carousel';
+import TimeUntil from './timeUntil';
 
 const Featured = () => {
     return (
         <div style={{position:'relative'}}>
             
             <Carousel />
-            
+
             <div className="artist_name">
                 <div className="wrapper">
                     Arianna Grande
                 </div>
             </div>
+            <TimeUntil />
         </div>
     );
 };
---
+++ src/components/featured/timeUntil.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import Slide from 'react-reveal/Slide';
+
+class TimeUntil extends Component {
+
+    state = {
+        deadline:'Jan, 24, 2020',
+        days:'0',
+        hours:'0',
+        minutes:'0',
+        seconds:'0'
+    }
+
+    getTimeUntil(deadline){
+        const time = Date.parse(deadline) - Date.parse(new Date());
+        if(time < 0 ){
+
+        } else{
+            const seconds = Math.floor((time/1000)%60);
+            const minutes = Math.floor((time/1000/60)%60);
+            const hours = Math.floor((time/(1000*60*60))%24);
+            const days = Math.floor(time/(1000*60*60*24));
+            this.setState({
+                days,
+                minutes,
+                hours,
+                seconds
+            }
+            )
+        }
+
+    }
+
+    componentDidMount(){
+        setInterval(()=>this.getTimeUntil(this.state.deadline) , 1000)
+    }
+    render() {
+
+        const {days , minutes , seconds , hours} = this.state;
+        
+        return (
+            <Slide left delay={1000}>
+            <div className="countdown_wrapper">
+                <div className="countdown_top">
+                    Event starts in
+                </div>
+                <div className="countdown_bottom">
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                           { days } 
+                        </div>
+                        <div className="countdown_tag">
+                            Days
+                        </div>
+                    </div>
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            {hours}
+                        </div>
+                        <div className="countdown_tag">
+                            Hrs
+                        </div>
+                    </div>
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            {minutes}
+                        </div>
+                        <div className="countdown_tag">
+                            Mins
+                        </div>
+                    </div>
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            {seconds}
+                        </div>
+                        <div className="countdown_tag">
+                            Secs
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            </Slide>
+        );
+    }
+}
+
+export default TimeUntil;
